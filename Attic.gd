63a02e2d8b306742c0f278f1dd57bbae4f19613e
JohnLogostini <johnlogostini16485@gmail.com>
2026-05-26 02:47:48 -0300

    Fixed incorrect paths to the floating artifacts.

--- Attic.gd
+++ Attic.gd
@@ -8,6 +8,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$"Geometry/Floating Light Path/Path Follow 01".progress += 4.5 * delta
-	$"Geometry/Floating Light Path/Path Follow 02".progress += 4.0 * delta
-	$"Geometry/Floating Light Path/Path Follow 03".progress += 3.5 * delta
+	$"Floating Light Path/Path Follow 01".progress += 4.5 * delta
+	$"Floating Light Path/Path Follow 02".progress += 4.0 * delta
+	$"Floating Light Path/Path Follow 03".progress += 3.5 * delta
